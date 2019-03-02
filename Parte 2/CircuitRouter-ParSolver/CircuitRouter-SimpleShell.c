706d6fceb6bfdd5c3b5d6cfc4860b4be9c9c0feb
AndreAugusto11 <45052703+AndreAugusto11@users.noreply.github.com>
2019-03-02 17:18:44 +0000

    Add files via upload

---
+++ Parte 2/CircuitRouter-ParSolver/CircuitRouter-SimpleShell.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include "../lib/commandlinereader.h"
+#include <sys/wait.h>
+#include "errno.h"
+#include "assert.h"
+
+#define bufferSize 256
+#define numberMaxArgs 5
+
+typedef struct process{
+	pid_t pid;
+	int status;
+} Process;
+
+
+int main(int argc, char const *argv[])
+{
+	long MAXCHILDREN = -1;
+	int i = 0, status = 0;
+	int count = 1;						/* Total number of processes */
+	int numberProcesses = 0;			/* Number of active child processes */
+	long vectorCapacity = 10;			/* Vector Capacity */
+	pid_t pid = 0;
+	char line[bufferSize], *args[numberMaxArgs];
+
+	Process *vectorProcess = (struct process*)malloc(sizeof(Process) * vectorCapacity);			/* Alloc memory for an array of Processes */
+	assert(vectorProcess);
+
+	if (argc == 2){
+		if (argv[1] > 0)
+			MAXCHILDREN = atoi(argv[1]);							/* Sets MAXCHILDREN to the argument read in the command line */
+
+		else{
+			free(vectorProcess);
+			exit(1);
+		}
+	}
+
+	else if (argc > 2){
+		free(vectorProcess);
+		exit(1);
+	}
+
+	while (1){
+		while (readLineArguments(args, numberMaxArgs, line, bufferSize) == 0){}
+		
+		if (!strcmp(args[0], "run")){
+			if (args[1] == NULL)
+				continue;
+
+			if (count == vectorCapacity - 1) {						/* If the array is almost full it's time to realoc to a new size */
+				vectorCapacity *= 2;
+				vectorProcess = realloc(vectorProcess, sizeof(Process*) * vectorCapacity);
+				assert(vectorProcess);
+			}
+
+			if (MAXCHILDREN != -1){									/* If there's a limit to the number of active processes waits for a process to end */
+				if (numberProcesses == MAXCHILDREN){
+					pid = wait(&status);
+					if ((pid == -1) && (errno == EINTR))
+						while (errno == EINTR)
+							pid = wait(&status);
+					vectorProcess[i].pid = pid;
+					vectorProcess[i++].status = status;
+					numberProcesses--;
+				}
+			}
+
+			pid = fork();											/* Creates a new child process */
+
+
+			if (pid < 0){
+				perror("fork");
+				free(vectorProcess);
+				exit(1);
+			}
+
+			if (pid == 0){
+				printf("SOU FILHO DO PID %d. TENHO PID %d.\n", getppid(), getpid());
+				execl("CircuitRouter-SeqSolver", "CircuitRouter-SeqSolver", args[1], NULL);
+				perror("execl");
+				free(vectorProcess);
+				exit(1);
+			}
+			else {
+				if ((pid % 2) != 0) /* Pid do filho impar */
+					printf("NOVO FILHO COM PID IMPAR: %d\n", pid);
+
+				count++;
+				numberProcesses++;
+			}
+		}
+
+		if (!strcmp(args[0], "exit")){
+			while (pid != -1){
+				pid = wait(&status);							/* Waits for all the processes to terminate */
+				if ((pid == -1) && (errno == EINTR))			/* Confirms if there was an error in wait() */
+					continue;
+
+				vectorProcess[i].pid = pid;						/* Add to the array the process id and status */
+				vectorProcess[i++].status = status;
+			}
+
+			for(i = 0; vectorProcess[i].pid != -1; i++) {
+				if (vectorProcess[i].status == 0)
+					printf("CHILD EXITED (PID=%d; return OK)\n", vectorProcess[i].pid);
+				else
+					printf("CHILD EXITED (PID=%d; return NOK)\n", vectorProcess[i].pid);
+			}
+
+			free(vectorProcess);
+			exit(0);
+		}
+	}
+
+	return 0;
+}
---
+++ Parte 2/CircuitRouter-SeqSolver/CircuitRouter-SeqSolver.c
@@ -0,0 +1,341 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * This code is an adaptation of the Lee algorithm's implementation originally included in the STAMP Benchmark
+ * by Stanford University.
+ *
+ * The original copyright notice is included below.
+ *
+  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright (C) Stanford University, 2006.  All Rights Reserved.
+ * Author: Chi Cao Minh
+ *
+ * =============================================================================
+ *
+ * Unless otherwise noted, the following license applies to STAMP files:
+ *
+ * Copyright (c) 2007, Stanford University
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of Stanford University nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY STANFORD UNIVERSITY ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL STANFORD UNIVERSITY BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * =============================================================================
+ *
+ * CircuitRouter-SeqSolver.c
+ *
+ * =============================================================================
+ */
+
+
+#include <assert.h>
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lib/list.h"
+#include "maze.h"
+#include "router.h"
+#include "lib/timer.h"
+#include "lib/types.h"
+
+enum param_types {
+	PARAM_BENDCOST = (unsigned char)'b',
+	PARAM_XCOST    = (unsigned char)'x',
+	PARAM_YCOST    = (unsigned char)'y',
+	PARAM_ZCOST    = (unsigned char)'z',
+};
+
+enum param_defaults {
+	PARAM_DEFAULT_BENDCOST = 1,
+	PARAM_DEFAULT_XCOST    = 1,
+	PARAM_DEFAULT_YCOST    = 1,
+	PARAM_DEFAULT_ZCOST    = 2,
+};
+
+bool_t global_doPrint = FALSE;
+char* global_inputFile = NULL;
+long global_params[256]; /* 256 = ascii limit */
+
+
+/* =============================================================================
+ * displayUsage
+ * =============================================================================
+ */
+static void displayUsage (const char* appName){
+	printf("Usage: %s [options]\n", appName);
+	puts("\nOptions:                            (defaults)\n");
+	printf("    b <INT>    [b]end cost          (%i)\n", PARAM_DEFAULT_BENDCOST);
+	printf("    x <UINT>   [x] movement cost    (%i)\n", PARAM_DEFAULT_XCOST);
+	printf("    y <UINT>   [y] movement cost    (%i)\n", PARAM_DEFAULT_YCOST);
+	printf("    z <UINT>   [z] movement cost    (%i)\n", PARAM_DEFAULT_ZCOST);
+	printf("    h          [h]elp message       (false)\n");
+	exit(1);
+}
+
+
+/* =============================================================================
+ * setDefaultParams
+ * =============================================================================
+ */
+static void setDefaultParams (){
+	global_params[PARAM_BENDCOST] = PARAM_DEFAULT_BENDCOST;
+	global_params[PARAM_XCOST]    = PARAM_DEFAULT_XCOST;
+	global_params[PARAM_YCOST]    = PARAM_DEFAULT_YCOST;
+	global_params[PARAM_ZCOST]    = PARAM_DEFAULT_ZCOST;
+}
+
+
+/* =============================================================================
+ * doesFileExist
+ * =============================================================================
+ */
+int doesFileExist(char *fileName){
+	/* Returns 1 if there's a file it fileName, else returns 0 */
+
+	FILE *fp;
+
+	if ((fp = fopen(fileName, "r+")) == NULL)
+		return 0;
+	else {
+		fclose(fp);
+		return 1;
+	}   
+}
+
+
+/* =============================================================================
+ * parseArgs
+ * =============================================================================
+ */
+static void parseArgs (long argc, char* const argv[]){
+	long i;
+	long opt;
+	int countsFiles = 0;
+
+	opterr = 0;
+
+	setDefaultParams();
+
+	while ((opt = getopt(argc, argv, "hb:x:y:z:")) != -1) {
+		switch (opt) {
+			case 'b':
+			case 'x':
+			case 'y':
+			case 'z':
+				global_params[(unsigned char)opt] = atol(optarg);
+				break;
+			case '?':
+			case 'h':
+			default:
+				opterr++;
+				break;
+		}
+	}
+
+	for (i = optind; i < argc; i++) {
+		if (!doesFileExist(argv[i])){
+			fprintf(stderr, "Non-option argument: %s\n", argv[i]);
+			opterr++;
+		}
+		else
+			countsFiles++;
+
+	}
+		if (countsFiles > 1){					/* If there's more than one file in the arguments read in the command line */
+			fprintf(stderr, "Non-option argument: %s\n", argv[i]);
+			opterr++;
+		}
+
+	if (opterr) {
+		displayUsage(argv[0]);
+	}
+}
+
+
+/* =============================================================================
+ * openFiles
+ * =============================================================================
+ */
+void openFiles(FILE **fpIn, FILE **fpOut, char** argv, int argc){
+	/* Opens the input file and generates the output file */
+	char fileName1[50] = "";
+	char fileName2[50] = "";
+	char fileNameAux[50] = "";
+	int i = 0;
+	int indexFile = 0;
+
+	for (i = 0; i < argc; i++){
+		if (doesFileExist(argv[i]))
+			indexFile = i;					/* Checks the index of the file in argv */
+	}
+
+
+	if ((*fpIn = fopen(argv[indexFile], "r+")) == NULL) {				/* Invalid input file */
+		perror(argv[indexFile]);
+		exit(1);
+	}	
+
+	/* Generating the name of the output file */
+	strcpy(fileName1, argv[indexFile]);
+	strcpy(fileName2, argv[indexFile]);
+
+	strcat(fileName1, ".res");
+	strcat(fileName2, ".res.old");
+
+
+	if (!doesFileExist(fileName1)){
+		*fpOut = fopen(fileName1, "w");
+		assert(fpOut);
+		if (*fpOut == NULL){
+			perror(fileName1);
+			exit(1);
+		}
+	}
+
+	else {
+		if (!doesFileExist(fileName2)){
+			if (rename(fileName1, fileName2) != 0){
+				perror("rename");
+				exit(1);
+			}	
+			*fpOut = fopen(fileName1, "w");
+			assert(fpOut);
+			if (*fpOut == NULL){
+				perror(fileName1);
+				exit(1);
+			}
+		}
+		
+		else {
+			/* If the output files already exist rename ".res" to ".res.old" and opens the ".res" as the output file */
+
+			if (rename(fileName1, fileName2) != 0){
+				perror("rename");
+				exit(1);
+			}	
+
+			strcpy(fileNameAux, argv[indexFile]);
+			strcat(fileNameAux, ".res");
+
+			*fpOut = fopen(fileNameAux, "w");
+			assert(fpOut);
+			if (*fpOut == NULL){
+				perror(fileName1);
+				exit(1);
+			}
+		}
+	}
+}
+
+
+/* =============================================================================
+ * main
+ * =============================================================================
+ */
+int main(int argc, char** argv){
+	/*
+	 * Initialization
+	 */
+	FILE *fpIn = NULL;
+	FILE *fpOut = NULL;
+
+
+	/* The filename is required */
+	if (argc == 1)
+		exit(1);
+
+	parseArgs(argc, (char** const)argv);
+
+	openFiles(&fpIn, &fpOut, argv, argc);
+
+	maze_t* mazePtr = maze_alloc();
+	assert(mazePtr);
+	long numPathToRoute = maze_read(mazePtr, fpIn, fpOut);
+	router_t* routerPtr = router_alloc(global_params[PARAM_XCOST],
+									   global_params[PARAM_YCOST],
+									   global_params[PARAM_ZCOST],
+									   global_params[PARAM_BENDCOST]);
+	assert(routerPtr);
+	list_t* pathVectorListPtr = list_alloc(NULL);
+	assert(pathVectorListPtr);
+
+	router_solve_arg_t routerArg = {routerPtr, mazePtr, pathVectorListPtr};
+	TIMER_T startTime;
+	TIMER_READ(startTime);
+
+	router_solve((void *)&routerArg);
+
+	TIMER_T stopTime;
+	TIMER_READ(stopTime);
+
+	long numPathRouted = 0;
+	list_iter_t it;
+	list_iter_reset(&it, pathVectorListPtr);
+	while (list_iter_hasNext(&it, pathVectorListPtr)) {
+		vector_t* pathVectorPtr = (vector_t*)list_iter_next(&it, pathVectorListPtr);
+		numPathRouted += vector_getSize(pathVectorPtr);
+	}
+	fprintf(fpOut, "Paths routed    = %li\n", numPathRouted);
+	fprintf(fpOut, "Elapsed time    = %f seconds\n", TIMER_DIFF_SECONDS(startTime, stopTime));
+
+
+	/*
+	 * Check solution and clean up
+	 */
+	assert(numPathRouted <= numPathToRoute);
+	bool_t status = maze_checkPaths(mazePtr, pathVectorListPtr, fpOut);
+	assert(status == TRUE);
+	fputs("Verification passed.\n", fpOut);
+
+	maze_free(mazePtr);
+	router_free(routerPtr);
+
+	list_iter_reset(&it, pathVectorListPtr);
+	while (list_iter_hasNext(&it, pathVectorListPtr)) {
+		vector_t* pathVectorPtr = (vector_t*)list_iter_next(&it, pathVectorListPtr);
+		vector_t* v;
+		while((v = vector_popBack(pathVectorPtr))) {
+			// v stores pointers to longs stored elsewhere; no need to free them here
+			vector_free(v);
+		}
+		vector_free(pathVectorPtr);
+	}
+	list_free(pathVectorListPtr);
+
+	fflush(fpOut);
+	fclose(fpOut);
+
+	exit(0);
+}
+
+
+/* =============================================================================
+ *
+ * End of CircuitRouter-SeqSolver.c
+ *
+ * =============================================================================
+ */
